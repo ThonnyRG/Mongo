23146976b67091dbd1c883e0aa924a001d1487e8
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-11 14:48:17 -0600

    Correction in the class

--- movies/src/main/java/dev/tony/movies/Movie.java
+++ movies/src/main/java/dev/tony/movies/Movie.java
@@ -2,30 +2,40 @@ package dev.tony.movies;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Movie {
     @Id
     private ObjectId id;
     private String imdbId;
+    private String title;
     private String releaseDate;
     private String trailerLink;
     private String poster;
-    private List<String> generes;
     private List<String> backdrops;
+    private List<String> genres;
     @DocumentReference
-    private List<Review> reviewIds;
+    private List<Review> reviews;
 
+    public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres) {
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.genres = genres;
+    }
 }
