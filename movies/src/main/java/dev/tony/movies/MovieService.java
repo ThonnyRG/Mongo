124bec9f3e0917d32eef5064addf4ff35ec0b308
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-11 14:50:40 -0600

    Correction in the variable names

--- movies/src/main/java/dev/tony/movies/MovieService.java
+++ movies/src/main/java/dev/tony/movies/MovieService.java
@@ -1,15 +1,21 @@
 package dev.tony.movies;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MovieService {
+
     @Autowired
-    private MovieRepository movieRepository;
-    public List<Movie> allMovies() {
-        return movieRepository.findAll();
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+    public Optional<Movie> findMovieByImdbId(String imdbId) {
+        return repository.findMovieByImdbId(imdbId);
     }
 }
