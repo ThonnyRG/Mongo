f061d182f3688a1a07812555bfce4b37375c080a
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-11 20:03:24 -0600

    Fixed attributes

--- movies/src/main/java/dev/tony/movies/Movie.java
+++ movies/src/main/java/dev/tony/movies/Movie.java
@@ -10,8 +10,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import java.util.List;
-
 @Document(collection = "movies")
 @Data
 @AllArgsConstructor
